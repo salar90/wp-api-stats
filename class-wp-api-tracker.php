128449deba668660ffe6afbe57a790c9e3efab8d
Salar Gholizadeh <salar.g@gmail.com>
2019-03-24 13:08:39 +0430

    setup menu page

--- class-wp-api-tracker.php
+++ class-wp-api-tracker.php
@@ -1,7 +1,27 @@
 <?php
 class SG_API_Tracker{
-	function __construct() {
+	private $settings = [];
 
+	function __construct() {
+		$this->settings["menu_mode"] = "SUBMENU";
 	}
+
+	function register_hooks(){
+		add_action("admin_menu",[$this,"admin_menu"]);
+	}
+
+	function admin_menu(){
+		if( $this->settings['menu_mode'] == "SUBMENU" ){
+			add_submenu_page('tools.php' , __("API Tracker","api-tracker") , __("API Tracker","api-tracker") , 'manage_options' , 'api-tracker' , [$this,"admin_page"] );
+		}else{
+			add_menu_page(__("API Tracker","api-tracker") , __("API Tracker","api-tracker") , "manage_options" , 'api-tracker' , [$this,"admin_page"] );
+		}
+	}
+
+	function admin_page(){
+		echo "Hello World";
+	}
+
+
 
 }

5800501f20f3013c40cebfe8f981f0f55fd5fd90
Salar Gholizadeh <salar.g@gmail.com>
2019-03-24 18:18:21 +0430

    hook the track!

--- class-wp-api-tracker.php
+++ class-wp-api-tracker.php
@@ -13,7 +13,52 @@ class SG_API_Tracker{
 
 	function register_hooks(){
 		add_action("admin_menu",[$this,"admin_menu"]);
+//		add_filter( 'rest_pre_serve_request' , [$this,'track'] , 5 , 3 );
+		add_filter( 'rest_post_dispatch' , [$this,'track'] , 5 , 3 );
 	}
+
+	function track( $response, WP_REST_Server $handler, WP_REST_Request $request ){
+
+		$current_minute_json = get_option("sg_api_tracker_minute" , "[]");
+		$current_minute = json_decode($current_minute_json , true);
+		if(empty($current_minute)){
+			$current_minute = [];
+		}
+		$time = time();
+		$modified = false;
+		foreach ($current_minute as $key => $entry_data){
+			if($entry_data['time'] < $time - 60){
+				unset($current_minute[$key]);
+				$modified = true;
+			}
+		}
+		if($modified){
+			$current_minute = array_values($current_minute);
+		}
+
+		$response_status = 0;
+		if(is_a($response,WP_HTTP_Response::class)){
+			$response_status = $response->get_status();
+		}
+		if(is_a($response,WP_Error::class)){
+			$response_status = $response->get_error_code();
+		}
+
+
+		$new_entry = [
+			'method' => $request->get_method(),
+			'route' => $request->get_route(),
+			'response_status' => $response_status,
+			'time' => $time
+		];
+
+		$current_minute[] = $new_entry;
+
+		$current_minute_json = json_encode($current_minute);
+		update_option( 'sg_api_tracker_minute' ,$current_minute_json ,false);
+		return $response;
+	}
+
 
 	function admin_menu(){
 		if( $this->settings['menu_mode'] == "SUBMENU" ){
@@ -24,7 +69,12 @@ class SG_API_Tracker{
 	}
 
 	function admin_page(){
+		$this->stats = $this->load_stats();
 		include __DIR__ . '/views/admin-panel.php';
+	}
+
+	function load_stats(){
+		$current_data = get_option("sg_api_tracker_requests",[]);
 	}
 
 
